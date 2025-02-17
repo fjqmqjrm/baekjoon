6481a6f49eec9ec044728b73bdd35972972359b3
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2025-02-17 10:11:59 +0900

    [Gold IV] Title: 이분 그래프, Time: 844 ms, Memory: 201596 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -4,48 +4,6 @@ k = int(input())
 ans = []
             
 def bfs(links, start):
-    global visited
-    queue = deque()
-    queue.append(start)
-    visited[start] = True
-    while queue:
-        this_node = queue.popleft()
-
-        for node in links[this_node]:
-            if not visited[node]:
-                visited[node] = True
-                queue.append(node)
-    return 1
-
-'''
-for _ in range(k):
-    count = 0
-    v, e = map(int, input().split())
-    links = [[] for _ in range(v+1)]
-    visited = [False] * (v+1)
-    for _ in range(e):
-        node1, node2 = map(int, input().split())
-      
-        for notLinkedNode in range(v+1):
-           
-            if notLinkedNode != node1 and notLinkedNode != node2:
-                links[node1].append(notLinkedNode)
-                links[node2].append(notLinkedNode)
-    
-    for i in range(1, v+1):
-        if not visited[i]:
-            count += bfs(links,i)
-    
-    if count == 1 or count == 2:
-        ans.append('YES')
-    else:
-        ans.append('NO')
-    print(count)
-            
-for k in ans:
-    print(k)'''
-
-def reBfs(links, start):
     global teams
     queue = deque()
     queue.append((start,1))
@@ -60,6 +18,7 @@ def reBfs(links, start):
                 elif this_teamNum == 2:
                     teams[node] = 1
                     queue.append((node,1))
+                    
             elif teams[node] == this_teamNum:
                 return False
             else:
@@ -81,15 +40,8 @@ for _ in range(k):
     m = 1
     for i in range(1, v+1):
         if teams[i] == 0:
-            flag = reBfs(links2, i)
+            flag = bfs(links2, i)
             if not flag:
                 break
             
     print("YES" if flag else "NO")
-
-
-    
-
-    
-   
-            
